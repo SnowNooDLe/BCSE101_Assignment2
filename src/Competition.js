eeed148a1c8b07a0af99fc1c4aebb29d84ca4e90
SnowNooDLe <sdw123123@gmail.com>
2018-09-14 14:17:13 +1200

    working fine, splitted into each class files

--- src/Competition.js
+++ src/Competition.js
@@ -1,56 +1,3 @@
-class View {
-  static BLANK () {
-    return ''
-  }
-  static SPACE () {
-    return '&nbsp;'
-  }
-  static TAB () {
-    return '&nbsp;&nbsp;&nbsp;&nbsp;'
-  }
-  static NEWLINE () {
-    return '<br>'
-  }
-  static clr () {
-    document.body.style.fontFamily = 'Courier New'
-    document.body.innerHTML = ''
-  }
-  static out (newText) {
-    document.body.innerHTML += newText
-  }
-  static add (newText) {
-    document.body.innerHTML += '<br>' + newText
-  }
-}
-
-class Team {
-	constructor (newRank = 0, newName = 'unnamed', newVenue = 'place', newCity = 'city') {
-		this.rank = newRank
-		this.name = newName
-		this.venue = newVenue
-		this.city = newCity
-	}
-
-	toString () {
-    let result = `${this.name} is at rank @ ( ${this.rank} ) with venue ${this.venue} from city ${this.city}`
-    return result
-	}
-}
-
-class Game {
-	constructor (newWeek = 0, newHomeTeamRank = 0, newAwayTeamRank = 0, newDateTime = 'untimed'){
-		this.week = newWeek
-		this.homeTeamRank = newHomeTeamRank
-		this.awayTeamRank = newAwayTeamRank
-		this.dateTime = newDateTime
-	}
-
-	toString () {
-    let result = `@ ${this.week} there is a game between ( ${this.homeTeamRank} ) versus $${this.awayTeamRank} on ${this.dateTime}`
-    return result
-	}
-}
-
 class Competition {
 	constructor (newTitle, newYear) {
 		this.title = newTitle
@@ -66,17 +13,19 @@ class Competition {
 
 	getTeams() {
 		let result = `TEAMS ${View.NEWLINE()}`
-		let result2 = `Premiership Division ${View.NEWLINE()}`
 		console.log("testing")
 		console.log(this.allTeams[0].rank)
+		console.log(this.allTeams.length / 2)
 		for (let aTeam of this.allTeams){
-			console.log(aTeam[0])
-			if (aTeam.rank % 2 === 0){
-					result += View.TAB() + aTeam + View.NEWLINE()
+			if (aTeam.rank === 1){
+				result += `Premiership Division ${View.NEWLINE()}`
 			}
+			else if (aTeam.rank === 8){
+				result += `Championship Division ${View.NEWLINE()}`
+			}
+			result += aTeam + `${View.NEWLINE()}`
 
 		}
-
 		return result
 	}
 
